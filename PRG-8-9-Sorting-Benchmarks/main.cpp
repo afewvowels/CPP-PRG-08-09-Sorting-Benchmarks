aeba53d50ef4bc5887b20565073d3380abd2fe0a
Keith Smith <aeiouandsometimesy@me.com>
2017-10-09 20:00:24 -0400

    Initial Commit

---
+++ PRG-8-9-Sorting-Benchmarks/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PRG-8-9-Sorting-Benchmarks
+//
+//  Created by Keith Smith on 10/9/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
